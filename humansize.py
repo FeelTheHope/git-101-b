3ce6d2bdbbc475cdf586eb8f067b8f31977a4885
Marco Iafrati <marco.iafrati@gmail.com>
2019-03-04 18:35:15 +0100

    error handling

--- humansize.py
+++ humansize.py
@@ -2,8 +2,11 @@ SUFFIXES = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
 
 def approximate_size(size):
+    if size < 0:
+        raise ValueError('Number must be non-negative')
     multiple = 1024
     for suffix in SUFFIXES:
         size /= SUFFIXES[multiple]
         if size < multiple:
-            return f'{size} {suffixe}'        
+            return f'{size} {suffixe}'
+    raise ValueError('Number too large')
